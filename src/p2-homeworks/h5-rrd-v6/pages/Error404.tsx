255b61e2d5f98f865379b5c87bd99aa171452811
Vadim <vady.tih@gmail.com>
2022-05-04 20:19:48 +0300

    final Игнат ДЗ5

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <br/>
-            <br/>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.wrap}>
+            <div className={s.logo}>
+                <h1>404</h1>
+                <p> Sorry - File not Found!</p>
+                <div className={s.sub}>
+                    <p><a href="#">Вернуться на главную</a></p>
+                </div>
+            </div>
         </div>
     )
 }
